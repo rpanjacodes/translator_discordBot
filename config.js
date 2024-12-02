8d9f12c055e005f68afc63dec23673522a3ff442
r.panja codes <101450382+rpanjacodes@users.noreply.github.com>
2024-12-02 23:45:08 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+//(C) 2024 R.PanjaCodes
+module.exports = {
+    bot: {
+        token: '', //Your bot token - https://tinyurl.com/discordbot-token
+        prefix: '', //Your custom prefix of the bot, like "!" or "."
+        status: '', //Custom activity/status text
+        activity: '' //You can choose: PLAYING, LISTENING, WATCHING, COMPETING
+    }
+};
